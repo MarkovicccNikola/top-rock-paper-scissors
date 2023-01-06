02a03cb2999f1f17d2bd7182db5f92d62c77ae5d
Nikola <markovi3c.nikola@gmail.com>
2023-01-06 20:11:55 +0100

    Almost finished project as I wasn't commiting regularly, I was struggling with some code and forgot

--- script.js
+++ script.js
@@ -1,11 +1,63 @@
+let value1;
+let computerChoice;
+let playerChoice;
+let round = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+function generateRandomInteger(max) {
+    return Math.floor(Math.random() * max) + 1;
+}
+
 function getComputerChoice() {
-    const moves = ["rock", "paper", "scissors"];
-    const random = Math.floor(Math.random() * moves.length);
-    console.log(moves[random]);
+    value1 = generateRandomInteger(3);
+
+    switch (value1) {
+        case 1:
+            computerChoice = "rock";
+            break;
+        case 2:
+            computerChoice = "paper";
+            break;
+        case 3:
+            computerChoice = "scissors";
+            break;
+    }
 }
 
-playerSelection = window.prompt('choose: rock, paper, scissors').toLowerCase();
+function getPlayerChoice() {
+    playerChoice = window.prompt("enter rock, paper or scissors:").toLowerCase();
+}
 
-function playRound(playerSelection, computerSelection){
+function playRound() {
+
+    getComputerChoice();
+    getPlayerChoice();
+
+    if (computerChoice === playerChoice) {
+        console.log("draw");
+        return (++round);
+    } else if (computerChoice === "rock" && playerChoice === "paper") {
+        console.log("player wins");
+        return (++round, ++playerScore);
+    } else if (computerChoice === "paper" && playerChoice === "scissors") {
+        console.log("player wins");
+        return (++round, ++playerScore);
+    } else if (computerChoice === "scissors" && playerChoice === "rock") {
+        console.log("player wins");
+        return (++round, ++playerScore);
+    } else {
+        console.log("computer wins");
+        return (++round, ++computerScore);
+    }   
 
 }
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+    console.log(computerScore, playerScore);
+}
+
+playGame(); 
